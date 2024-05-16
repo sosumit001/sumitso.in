fccd4810cf8d7e30746e8b51bf6d9679548f9b1b
sumitm001 <sumit.m@reidnax.com>
2024-05-16 19:02:19 +0530

    feat: blog-thumbnails

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import { formatDate, getBlogPosts } from '../blogs/utils'
 import { imprima } from '../lib/fonts'
+import Image from 'next/image'
 
 export function BlogPosts() {
   let allBlogs = getBlogPosts()
@@ -19,22 +20,44 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex gap-0 px-3 sm:px-4 md:px-5 lg:px-6 shadow-slate-700 bg-[#fdfff8]"
+            className="flex gap-0 px-3 flex-wrap sm:px-4 md:px-5 lg:px-6 shadow-slate-700 bg-[#ededed]"
             href={`/blogs/${post.slug}`}
           >
-            <div className="w-full flex gap-3 flex-col my-4 relative">
-              <div className='flex gap-4 flex-wrap'>
-                <div className="text-neutral-900 tracking-tight">
-                 {post.metadata.title}
-                </div>
-                <div className="text-neutral-500 w-[150px]">
-                  {formatDate(post.metadata.publishedAt, false)}
-                </div>
-              </div>
+            <div className="w-full flex items-center flex-wrap my-2 relative">
 
-              <div className="text-neutral-600 tracking-tight">
-                {post.metadata.summary}
-              </div>
+                <div className="text-neutral-600 tracking-tight">
+
+                  <div className='flex items-center px-4 justify-center max-sm:flex-wrap max-md:py-4 max-sm:py-4'>
+
+                    {/* left */}
+                    <div>
+                      {/* title */}
+                      <div className='flex gap-2 flex-col max-sm:gap-1'>
+                        <div className="text-neutral-900 max-sm:px-2 tracking-tight">
+                          {post.metadata.title}
+                        </div>
+                        <div className="text-neutral-400 w-[150px]">
+                          {formatDate(post.metadata.publishedAt, false)}
+                        </div>
+                      </div>
+                      {/* summary */}
+                      <div className='text-left mt-3 pr-16 max-sm:px-2'>
+                        {post.metadata.summary}
+                      </div>
+                    </div>
+
+                    {/* image */}
+                    <Image
+                    src={`/blogs/thumbnails/${post.slug}.png`}
+                    alt={`${post.slug}`}
+                    height={40}
+                    width={200}
+                    priority
+                    className='max-sm:w-[90%]'
+                    />
+
+                  </div>
+                </div>
             </div>
           </Link>
         ))}
