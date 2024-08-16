561dd15ea52afafbb8af3e47325b96549fa77452
sumitm001 <sumit.m@reidnax.com>
2024-08-16 20:06:51 +0530

    chore(commitlint, husky): enabled commitlint with husky

---
+++ commitlint.config.ts
@@ -0,0 +1,25 @@
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',     // New features
+				'fix',      // Bug fixes
+				'chore',    // Maintenance tasks
+				'ci',       // CI configuration changes
+				'docs',     // Documentation updates
+				'perf',     // Performance improvements
+				'refactor', // Code refactoring
+				'revert',   // Revert to previous commit
+				'style',    // Code style changes
+				'test',     // Adding or updating tests
+			],
+		],
+	}
+};
+
+module.exports = Configuration;
