b6c32b8322a80630936be3b34540defc358f89b6
Никитин Руслан Романович <RRNikitin@alfabank.ru>
2025-01-11 20:36:52 +0300

    chore(commitlint, husky): enable commitlint with husky

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,25 +1,26 @@
 import type { UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
-	extends: ["@commitlint/config-conventional"],
-	rules: {
-		'type-enum': [
-			2,
-			'always',
-			[
-				'feat',     // New features
-				'fix',      // Bug fixes
-				'chore',    // Maintenance tasks
-				'ci',       // CI configuration changes
-				'docs',     // Documentation updates
-				'perf',     // Performance improvements
-				'refactor', // Code refactoring
-				'revert',   // Revert to previous commit
-				'style',    // Code style changes
-				'test',     // Adding or updating tests
-			],
-		],
-	}
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New features
+        "fix", // Bug fixes
+        "chore", // Maintenance tasks
+        "ci", // CI configuration changes
+        "docs", // Documentation updates
+        "perf", // Performance improvements
+        "refactor", // Code refactoring
+        "revert", // Revert to previous commit
+        "style", // Code style changes
+        "test", // Adding or updating tests
+        "explora", // New Exploration
+      ],
+    ],
+  },
 };
 
 module.exports = Configuration;
